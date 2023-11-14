0be2373172d60043d4bdbc9c4ce4b7a338900b46
Sylwia Ch <chedkowska0@gmail.com>
2023-11-14 20:19:17 +0100

    wstepnie poprawione dodawanie

--- form_dodaj.php
+++ form_dodaj.php
@@ -6,14 +6,18 @@
         $opis = $_REQUEST['opis'];
 
         $sql = "INSERT INTO announcement(`title`, `park`, `date`, `description`) VALUES ('$tytul',$park,'$data','$opis') ";
-        $sql2 = "SELECT * FROM `announcement` WHERE title='$tytul' && park='$park' && date='$data'";
-        if($sql2='' && mysqli_query($conn, $sql) ){
+        $sql2 = "SELECT park FROM `announcement` WHERE title='$tytul' && park='$park' && date='$data'";
+        $result = mysqli_query($conn, $sql2);
+        $row =  mysqli_fetch_array($result); 
+       
+        if($row=='' && mysqli_query($conn, $sql) ){
             header("Location:ogloszenia.html");
             echo "<div><p>Dane zostały dodane do bazy danych. </p></div></br>"; 
  
             echo("\n$tytul\n$park\n$data\n$opis");
         } else{
-            echo "ERROR: $sql. ". mysqli_error($conn);
+            echo "ERROR: $sql2. ". mysqli_error($conn);
+            print_r ($row);
             echo "Dane już istnieją";
         }
         mysqli_close($conn);
